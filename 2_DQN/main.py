1b348e1810e89213016f5e7dc909cf5867c3e999
fcrozet <fcrozet@witekio.com>
2019-09-25 15:16:22 +0200

    Test the action step

--- 2_DQN/main.py
+++ 2_DQN/main.py
@@ -19,20 +19,26 @@ def main():
     print(state_size)
     print(action_size)
 
-    env.reset()
-    observation = env.step(0)
-    print(observation[0].shape)
+    for E in range(100):
 
-    #Grayscale the observation
-    observation_grayscale = np.dot(observation[0][...,:3],[0.3,0.6,0.1])
-    print(observation_grayscale.shape)
-    observation_grayscale_resize = np.resize(observation_grayscale, (110,84))
-    print(observation_grayscale_resize.shape)
+        env.reset()
+        observation = env.step(0)
+        print(observation[0].shape)
+        terminal = False
+        #Grayscale the observation
+        observation_grayscale = np.dot(observation[0][...,:3],[0.3,0.6,0.1])
+        print(observation_grayscale.shape)
+        observation_grayscale_resize = np.resize(observation_grayscale, (110,84))
+        print(observation_grayscale_resize.shape)
 
-    action = breakoutAgent.act(observation_grayscale_resize)
-    print(action)
-    state, reward, done, info = env.step(action)
-    print(state.shape)
+
+        while not terminal :
+            env.render()
+            action = breakoutAgent.act(observation_grayscale_resize)
+            state, reward, terminal, info = env.step(action)
+            observation_grayscale = np.dot(observation[0][...,:3],[0.3,0.6,0.1])
+            observation_grayscale_resize = np.resize(observation_grayscale, (110,84))
+
 
 if __name__ == "__main__":
     main()
